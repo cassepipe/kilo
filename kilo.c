32382aa086ad5be9e72e5d7a2fefe52563dd66d6
cassepipe <cassepipe@ymail.com>
2020-04-06 17:48:14 +0200

    Added the HOME and END keys (also from numpad)

--- kilo.c
+++ kilo.c
@@ -14,10 +14,15 @@
 #define CTRL_KEY(k)	((k) & 0x1f)
 
 enum editorKey {
+	DELETE_KEY = 999,
 	ARROW_UP = 1000,
 	ARROW_DOWN,
 	ARROW_RIGHT,
-	ARROW_LEFT
+	ARROW_LEFT,
+	HOME_KEY,
+	END_KEY,
+	PAGE_UP,
+	PAGE_DOWN
 };
 
 /***data***/
@@ -87,25 +92,60 @@ int editorReadKey()
 			die("read() failed in editorReadKey()");
 	}
 
-	if (c == '\x1b')		//Is is an escape sequence ?
+	if (c == '\x1b')		//Is it an escape sequence ?
 	{
 		char seq[3];
-		
-		if (read(STDIN_FILENO, seq, 1) != 1) return '\x1b';
-		if (read(STDIN_FILENO, seq + 1, 1) != 1) return '\x1b';
-		
-		if (seq[0] == '[') 
+
+		if (read(STDIN_FILENO, &seq[0], 1) != 1)
+			return '\x1b';
+		if (read(STDIN_FILENO, &seq[1], 1) != 1)
+			return '\x1b';
+
+		if (seq[0] == '[')
 		{
-			switch (seq[1])
+			if (seq[1] >= '0' && seq[1] <= '9')
+			{ 
+				if (read(STDIN_FILENO, &seq[2], 1) != 1)
+					return '\x1b';
+				if (seq[2] == '~')
+				{
+					switch (seq[1])
+					{
+						case '1': return HOME_KEY;
+						case '3': return DELETE_KEY;
+						case '4': return END_KEY;
+						case '5': return PAGE_UP;
+						case '6': return PAGE_DOWN;
+						case '7': return HOME_KEY;
+						case '8': return END_KEY;
+					}
+				}
+			}
+			else
 			{
-				case 'A': return ARROW_UP;
-				case 'B': return ARROW_DOWN;
-				case 'C': return ARROW_RIGHT;
-				case 'D': return ARROW_LEFT;
+				switch (seq[1])
+				{
+					case 'A': return ARROW_UP;
+					case 'B': return ARROW_DOWN;
+					case 'C': return ARROW_RIGHT;
+					case 'D': return ARROW_LEFT;
+					case 'H': return HOME_KEY;
+					case 'F': return END_KEY;
+				}
 			}
 			return '\x1b';
 		}
-	} else 
+		else if (seq[0] == 'O')
+		{
+			switch (seq[1])
+			{
+				case 'H': return HOME_KEY;
+				case 'F': return END_KEY;
+			}
+			return '\x1b';
+		}
+	} 
+	else 
 	{
 		return c;
 	}
@@ -182,24 +222,42 @@ void editorMoveCursor(int key)
 			if (E.cx > 0)
 				E.cx--;
 			break;
+		case HOME_KEY:
+			E.cx = 0;
+			break;
+		case END_KEY:
+			E.cx = E.screencols - 1;
+			break;
 	}
 }
-	
+
 void editorProcessKeypress()
 {
 	int c = editorReadKey() ;
 
 	switch (c) 
-	{	case CTRL_KEY('q'):
-		write(STDOUT_FILENO, "\x1b[2J", 4);
-		write(STDOUT_FILENO, "\x1b[H", 3);
-		exit(0) ;
-		
+	{	
+		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\x1b[2J", 4);
+			write(STDOUT_FILENO, "\x1b[H", 3);
+			exit(0) ;
+
 		case ARROW_UP:
 		case ARROW_DOWN:
 		case ARROW_RIGHT:
 		case ARROW_LEFT:
+		case HOME_KEY:
+		case END_KEY:
 			editorMoveCursor(c);
+			break;
+		case PAGE_UP:
+		case PAGE_DOWN:
+		{
+			int times = E.screenrows;
+			while (times--)
+				editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+			break;
+		}
 	}
 }
 
@@ -221,7 +279,7 @@ void editorRefreshScreen()
 	abAppend(&ab, "\x1b[H", 3);		//Put the cursor at origin
 
 	editorDrawRows(&ab);
-	
+
 	char buf[32];
 	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
 	abAppend(&ab, buf, strlen(buf));
@@ -247,7 +305,7 @@ int main()
 	enableRawMode();
 	initEditor();
 	//editorOpen();
-	
+
 	while(1)
 	{
 		editorRefreshScreen();
